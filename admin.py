54c14bc613f8b82b661ceecbce08c0a1660896ad
sloww <hust.sh@gmail.com>
2017-11-14 00:39:29 +0800

    m

--- admin.py
+++ admin.py
@@ -20,7 +20,7 @@ class QrLabelAdmin(SelectModelAdmin):
 class ScanRecordAdmin(SelectModelAdmin):
     search_fields = ('city',)
     list_display = ('ip','city','scan_date' )
-    fields = ('qr_label','ip','json', 'city', )
+    fields = ('ip','json', 'city', )
 
 class LabelRecordAdmin(SelectModelAdmin):
     search_fields = ('master_code',)
